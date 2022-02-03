a7fcb0f42bb3f4b943ed5ecb1dd2ae64e966dcc4
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-02-03 11:35:12 -0300

    Fundamento C#

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace CursoCSharp.Fundamentos
+
+{   /// <summary>
+    /// 
+    /// </summary>
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            // Cuidado com os comentarios desnecessarios
+            Console.WriteLine("Codigo claro é sempre melhor!");
+
+             /// 3 barras para gerar documentação
+            Console.WriteLine("O C# tem o XML Comments");
+
+            /* Padrão
+             * 
+             */
+
+            Console.WriteLine("Comentario de multiplas linhas");
+
+            Console.WriteLine("CTRL + U = Descomenta bloco de codigo!");
+            Console.WriteLine("CTRL + C = Comenta bloco de codigo!");
+        }
+    }
+}
---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace CursoCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+      public  static void Executar() // start do programa
+        {
+            Console.Write("Primeiro "); // escreve e mantem na mesma linha
+            Console.WriteLine("Programa"); // escreve e colocar o cursor na linha de baixo
+            Console.WriteLine("Terminou");
+            Console.WriteLine(); // cw + TAB atalho
+        }
+    }
+}
+
