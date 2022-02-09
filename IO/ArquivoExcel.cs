40e98fd76774fad0f9ca867621ccc5ffc808c73d
Wilson Generoso Junior <wilson.generoso@hotmail.com>
2022-02-09 15:04:32 -0300

    Leitor Xml e Escritor CSV

---
+++ IO/ArquivoExcel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CursoCSharp.IO
+{
+    /// <summary>
+    /// DESAFIO: Converter um arquivo XML para formato CSV
+    /// 
+    /// XML a ser utilizado:
+    /// </summary>
+    
+    public class LeitorXml
+    {
+        public static void Executar()
+        {
+            var xml = new XmlDocument();
+            xml.Load(@"");
+
+        }
+    }
+
+    public class EscreverCSV
+    {
+
+        public static void Executar()
+        {
+            // Namespace: System.IO
+            // Objetos: File, FileInfo, Directory, Path, Stream, StringWriter (escrever o arquivo), StringReader
+        }
+    }
+
+}
