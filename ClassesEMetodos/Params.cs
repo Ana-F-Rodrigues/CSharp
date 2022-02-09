ff5b22db54dbb9435daebf6c9c6673ac0ed33dac
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-02-09 09:18:14 -0300

    Params

---
+++ ClassesEMetodos/Params.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Params
+    {
+        //params = parametros variaveis - Agrupa varios valores dentro dela
+        // string[] array
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach(var pessoa in pessoas)
+            {
+                Console.WriteLine("Olá {0}",pessoa); // percorre todo o array
+            }
+        }
+
+        public static void Executar()
+        {
+            Recepcionar("Pedro", "Ana", "Joao");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -50,6 +50,8 @@ namespace CursoCSharp
                 {"Metodos com Retorno - Classes e Métodos",MetodosComRetorno.Executar},
                 {"Metodos Estaticos - Classes e Métodos",MetodosEstaticos.Executar},
                 {"Atributos Estaticos - Classes e Métodos",AtributosEstaticos.Executar},
+                {"Desafio Atributos - Classes e Métodos",DesafioAtributo.Executar},
+                {"Params- Classes e Métodos",Params.Executar},
 
             });
 
