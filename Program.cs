2c39ff20bd7eb8a4e6d63d4b0f7da3241557fb34
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-02-14 15:20:27 -0300

    list

--- Program.cs
+++ Program.cs
@@ -66,6 +66,7 @@ namespace CursoCSharp
                 //Coleçoes
                 
                 {"Array - Coleções",Colecoes.Array.Executar},
+                {"Lis - Coleções",ColecoesList.Executar},
 
             });
 
