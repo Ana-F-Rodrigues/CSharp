08f344b4023bd05c61da75a880b7a9278845dbe7
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-02-14 11:20:28 -0300

    Atribuição

--- ClassesEMetodos/StructVsClasse.cs
+++ ClassesEMetodos/StructVsClasse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CursoCSharp.ClassesEMetodos
 {
-    public struct Sponto
+    public struct SPonto
     {
         public int X;
         public int Y;
@@ -22,6 +22,19 @@ namespace CursoCSharp.ClassesEMetodos
     {
         public static void Executar()
         {
+            SPonto ponto1 = new SPonto { X = 1, Y = 3 };
+            SPonto copiaPonto1 = ponto1; // atribuição feita por valor
+            ponto1.X = 3;
+
+            Console.WriteLine("Ponto 1 X:{0}", ponto1.X);
+            Console.WriteLine("Copia Ponto 1 X:{0}",copiaPonto1.X);
+
+            CPonto ponto2 = new CPonto { X = 2, Y = 4 };
+            CPonto copiaPonto2 = ponto2; // por referencia apontam para o mesmo local
+            ponto2.X = 4;
+
+            Console.WriteLine("Ponto 2 X:{0}", ponto2.X);
+            Console.WriteLine("Copia Ponto 2 X:{0}", copiaPonto2.X);
 
         }
     }
