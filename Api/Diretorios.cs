62231f71a6fb5a4c1a2a4bf8f649a67d02ce6a87
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-02-17 08:50:23 -0300

    trabalhando com Diretorios

--- Api/Diretorios.cs
+++ Api/Diretorios.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CursoCSharp.Api
 {
@@ -10,6 +7,9 @@ namespace CursoCSharp.Api
     {
         public static void Executar()
         {
+            var novoDir = @"~/PastaCSharp".ParseHome();
+            var novoDirDestino = @"~/PastaCSharpeDestino".ParseHome();
+            
 
         }
     }
