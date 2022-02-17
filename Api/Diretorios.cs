350345d8413150a4e84dd6431ccf1d8bd6a2ca71
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-02-17 10:46:32 -0300

    manipulando Directory

--- Api/Diretorios.cs
+++ Api/Diretorios.cs
@@ -9,8 +9,39 @@ namespace CursoCSharp.Api
         {
             var novoDir = @"~/PastaCSharp".ParseHome();
             var novoDirDestino = @"~/PastaCSharpeDestino".ParseHome();
-            
+            var dirProjeto = @"C:\Users\Ana\Desktop\FULLSTACK\FullStack";
 
+            if(Directory.Exists(novoDir)) 
+            {
+                Directory.Delete(novoDir, true);
+            }
+
+            if(Directory.Exists(novoDirDestino))
+            {
+                Directory.Delete(novoDirDestino, true);
+            }
+
+            Directory.CreateDirectory(novoDir);
+            Console.WriteLine(Directory.GetCreationTime(novoDir));
+
+            Console.WriteLine("**** Pasta Listadas **********");
+            var pastas = Directory.GetDirectories(dirProjeto);
+            foreach(var pasta in pastas)
+            {
+                Console.WriteLine(pasta);
+            }
+
+            Console.WriteLine("\n\n***** Arquivos ******");
+            var arquivos = Directory.GetFiles(dirProjeto);
+            foreach (var arquivo in arquivos)
+            {
+                Console.WriteLine(arquivo);
+            }
+
+            Console.WriteLine("\n\n**** Pasta Raiz *****");
+            Console.WriteLine(Directory.GetDirectoryRoot(novoDir));
+
+            Directory.Move(novoDir, novoDirDestino);
         }
     }
 }
